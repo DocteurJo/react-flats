c3a66632209c892744c607c34a82591fc83f70b1
Joseph Scemama <j.scemama1@gmail.com>
2020-01-27 12:28:19 +0100

    settle new exercise

---
+++ src/components/app.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello,
+        Jo
+      </div>
+    );
+  }
+}
+
+export default App;
